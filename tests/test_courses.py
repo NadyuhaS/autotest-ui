a36821b500a69385d03bc327f52f384a840d5179
NadyuhaS <nadyuhastartseva@gmail.com>
2026-01-13 21:54:21 +0100

    Step 7.2 Page Object practice

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,31 +1,46 @@
 import pytest
 from playwright.sync_api import sync_playwright, expect
+from pages.courses_list_page import CoursesListPage, CheckVisibleCourseCardParams
+from pages.create_course_page import CreateCoursePage
 
-from tests.conftest import chromium_page_with_state
 
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list(chromium_page_with_state):
-    chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
-        # Нажимаем на кнопку Login
-
-        # Проверяем, что появилось сообщение об ошибке
-        # wrong_email_or_password_alert = page.get_by_test_id('login-page-wrong-email-or-password-alert')
-        # expect(wrong_email_or_password_alert).to_be_visible()
-        # expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
-
-    courses = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
-    expect(courses).to_have_text('Courses')
-    expect(courses).to_be_visible()
-
-    icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
-    expect(icon).to_be_visible()
-
-    result_text = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
-    expect(result_text).to_have_text('There is no results')
-    expect(result_text).to_be_visible()
-
-    paragraph_text = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
-    expect(paragraph_text).to_have_text('Results from the load test pipeline will be displayed here')
-    expect(paragraph_text).to_be_visible()
+def test_create_course(create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
+    create_course_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
+    create_course_page.check_visible_create_course_title()
+    create_course_page.check_disabled_create_course_button()
+    create_course_page.check_visible_image_preview_empty_view()
+    create_course_page.check_visible_image_upload_view()
+    create_course_page.check_visible_create_course_form(
+        title='',
+        estimated_time='',
+        description='',
+        max_score='0',
+        min_score='0'
+    )
+    create_course_page.check_visible_exercises_title()
+    create_course_page.check_visible_create_exercise_button()
+    create_course_page.check_visible_exercises_empty_view()
+    create_course_page.upload_preview_image('./testdata/files/image.png')
+    create_course_page.check_visible_image_upload_view(is_image_uploaded=True)
+    create_course_page.fill_create_course_form(
+        title='Playwright',
+        estimated_time='2 weeks',
+        description='Playwright',
+        max_score='100',
+        min_score='10'
+    )
+    create_course_page.click_create_course_button()
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_course_card(
+        CheckVisibleCourseCardParams(
+            index=0,
+            title="Playwright",
+            max_score="100",
+            min_score="10",
+            estimated_time="2 weeks"
+        )
+    )
+    courses_list_page.check_visible_create_course_button()
